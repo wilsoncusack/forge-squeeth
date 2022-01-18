ab1cee98ccf0d4bf92303a41551398a022e3e5a6
Wilson Cusack <cusackw@gmail.com>
2022-01-17 22:44:45 -0500

    made CastingTester work with fuzzer :)

--- src/test/CastingTester.sol
+++ src/test/CastingTester.sol
@@ -8,14 +8,17 @@ contract CastingTester{
     using Uint256Casting for uint256;
 
     function testToUint128(uint256 y) external pure returns (uint128 z) {
+        if(uint128(y) != y) return 0;
         return y.toUint128();
     }
 
     function testToUint96(uint256 y) external pure returns (uint96 z) {
+        if(uint96(y) != y) return 0;
         return y.toUint96();
     }
 
     function testToUint32(uint256 y) external pure returns (uint32 z) {
+        if(uint32(y) != y) return 0;
         return y.toUint32();
     }
 }
